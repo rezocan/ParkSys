3c90e05bf4bc4940a55262316354f32721311519
faezehshah <32835380+faezehshah@users.noreply.github.com>
2017-12-07 15:18:48 +0100

    Add files via upload

---
+++ Tiketif.java
@@ -0,0 +1,9 @@
+package Parkhaus;
+
+public interface Tiketif {
+	
+	 public void setEinfahrt( int time1,int time2);
+	 public void setAusfahrt( int time1, int time2);
+	 public int getGebuehren( int gebuehr);
+
+}
